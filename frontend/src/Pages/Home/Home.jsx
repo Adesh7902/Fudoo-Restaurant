96016c9214794ab9f69f26562d8172d6125800cd
Adesh Jadhav <adeshjadhav0709@gmail.com>
2024-07-08 23:50:21 +0530

    Day 2

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../Components/Header/Header";
 import ExploreMenu from "../../Components/Exploremenu/Exploremenu";
+import FoodDisplay from "../../Components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
+  const [category, setCategory] = useState("All");
   return (
     <div className="home">
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category}/>
     </div>
   );
 };
